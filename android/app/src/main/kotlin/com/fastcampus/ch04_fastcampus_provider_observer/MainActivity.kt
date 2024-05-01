7ecb257c2c9c66ee8fcf41c14ea7903d6666a0eb
Dreamwalker <aristojeff@gmail.com>
2024-05-01 17:26:18 +0900

    add

---
+++ android/app/src/main/kotlin/com/fastcampus/ch04_fastcampus_provider_observer/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.fastcampus.ch04_fastcampus_provider_observer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class ProviderLogger extends ProviderObserver {
+  @override
+  void didAddProvider(ProviderBase<Object?> provider, Object? value,
+      ProviderContainer container) {
+    // TODO: implement didAddProvider
+    // super.didAddProvider(provider, value, container);
+    print('''
+      {
+       "provider" : "${provider.name ?? provider.runtimeType}"
+       "value": "${value}"
+      }
+    ''');
+  }
+
+  @override
+  void didUpdateProvider(ProviderBase<Object?> provider, Object? previousValue,
+      Object? newValue, ProviderContainer container) {
+    // TODO: implement didUpdateProvider
+    // super.didUpdateProvider(provider, previousValue, newValue, container);
+    print('''
+    {
+      "provider" : "${provider.name ?? provider.runtimeType}",
+      "previousValue" : "${previousValue}",
+      "newValue" : "$newValue",
+    }
+    ''');
+  }
+  @override
+  void didDisposeProvider(ProviderBase<Object?> provider, ProviderContainer container) {
+    // TODO: implement didDisposeProvider
+    // super.didDisposeProvider(provider, container);
+
+    print('''
+    didDisposeProvider()
+    {
+      "provider" : "${provider.name ?? provider.runtimeType}",
+    }
+    ''');
+  }
+}
+
+final counterStateProvider =
+    StateProvider.autoDispose<int>((ref) => 0, name: "counterStateProvider");
+
+void main() {
+  runApp(
+    ProviderScope(
+      observers: [
+        ProviderLogger(),
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => SecondPage(),
+            ),
+          );
+        },
+        child: Text("페이지 이동"),
+      ),
+    ));
+  }
+}
+
+class SecondPage extends ConsumerWidget {
+  const SecondPage({super.key});
+
+  @override
+  Widget build(BuildContext context, ref) {
+    final counter = ref.watch(counterStateProvider);
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Text(
+          "$counter",
+          style: TextStyle(
+            fontSize: 24,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          ref.read(counterStateProvider.notifier).state++;
+        },
+      ),
+    );
+  }
+}
